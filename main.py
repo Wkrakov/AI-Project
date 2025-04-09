bd9ee66b49fd9d126036123b143fc5ac35b94369
Wkrakov <babzooles@mail.ru>
2025-04-09 19:31:30 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import io
 import streamlit as st
-from PIL import Image
-from paddleocr import PaddleOCR
 
-# Функция для загрузки изображения
+from transformers import pipeline
+from PIL import Image
+
+
 def load_image():
     uploaded_file = st.file_uploader(label='Выберите изображение для распознавания')
     if uploaded_file is not None:
@@ -13,21 +14,13 @@ def load_image():
     else:
         return None
 
-# Функция для распознавания текста с помощью Paddle OCR
-def ocr_with_paddle(img):
-    ocr = PaddleOCR(lang='en', use_angle_cls=True)
-    result = ocr.ocr(img)
-    final_text = ''
-    for item in result:
-        final_text += ' ' + item[1][0]
-    return final_text
 
 st.title('Распознай английский текст с изображения!')
 img = load_image()
 
-if img is not None:
-    result_button = st.button('Распознать изображение')
-    if result_button:
-        extracted_text = ocr_with_paddle(img)
-        st.write('Результаты распознавания:')
-        st.write(extracted_text)
+result = st.button('Распознать изображение')
+if result:
+    captioner = pipeline("image-to-text", model="YaelSch/OCR-image-to-text-m")
+    text = captioner(img)
+    st.write('Результаты распознавания:')
+    st.write(text[0]["generated_text"])
